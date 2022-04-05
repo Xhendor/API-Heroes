5489b552311e96bbfe3026495a071db9a652907d
xhendor <rosendorsc@gmail.com>
2022-04-05 11:34:55 -0700

    Init commit

---
+++ routes/heroes.js
@@ -0,0 +1,43 @@
+const express=require('express');
+const router= express.Router();
+const heroes=require('../services/heroes');
+
+router.get('/',async function(req,res,next){
+
+    try{
+        res.json(await heroes.getMultiple(req.query.page));
+    }catch(err){
+        console.error(`Error al obtener los lenguajes`,err.message);
+        next(err);
+    }
+});
+router.post('/',async function(req,res,next){
+    try{
+        console.error(req.body);
+        res.json(await heroes.create(req.body));
+    }catch(err){
+        console.error(`Error mientras se creaba el registro`,
+        err.message);
+        next(err);
+    }
+});
+router.put('/:id',async function(req,res,next){
+    try {
+        res.json(await heroes.update(
+            req.params.id,req.body))
+    } catch (error) {
+        console.error(`Error mientras se creaba el registro`,
+        err.message);
+        next(err);
+    }
+});
+router.delete('/:id',async function(req,res,next){
+    try {
+        res.json(await heroes.remove(
+            req.params.id));
+    } catch (error) {
+        console.error(`Error while deleting programming language`, err.message);
+        next(err);    
+    }
+});
+module.exports=router;
